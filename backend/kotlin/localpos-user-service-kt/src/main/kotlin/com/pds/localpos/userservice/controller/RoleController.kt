e5110a6d34061cfdab589483de9d1e591ac1a8a7
icastorena <ing.escapite@gmail.com>
2025-07-04 15:36:26 -0600

    POS002: Add logs

--- backend/kotlin/localpos-user-service-kt/src/main/kotlin/com/pds/localpos/userservice/controller/RoleController.kt
+++ backend/kotlin/localpos-user-service-kt/src/main/kotlin/com/pds/localpos/userservice/controller/RoleController.kt
@@ -2,6 +2,7 @@ package com.pds.localpos.userservice.controller
 
 import com.pds.localpos.userservice.dto.RoleDTO
 import com.pds.localpos.userservice.service.RoleService
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,9 +14,13 @@ class RoleController(
     private val roleService: RoleService
 ) {
 
+    private val logger = LoggerFactory.getLogger(RoleController::class.java)
+
     @GetMapping
     fun getAllRoles(): ResponseEntity<Set<RoleDTO>> {
+        logger.info("Fetching all roles")
         val roles = roleService.getAllRoles()
+        logger.info("Fetched {} roles", roles.size)
         return ResponseEntity.ok(roles)
     }
 }
--- backend/kotlin/localpos-user-service-kt/src/main/kotlin/com/pds/localpos/userservice/controller/StoreController.kt
+++ backend/kotlin/localpos-user-service-kt/src/main/kotlin/com/pds/localpos/userservice/controller/StoreController.kt
@@ -2,6 +2,7 @@ package com.pds.localpos.userservice.controller
 
 import com.pds.localpos.userservice.dto.StoreDTO
 import com.pds.localpos.userservice.service.StoreService
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,9 +14,13 @@ class StoreController(
     private val storeService: StoreService
 ) {
 
+    private val logger = LoggerFactory.getLogger(StoreController::class.java)
+
     @GetMapping
     fun getAllStores(): ResponseEntity<Set<StoreDTO>> {
+        logger.info("Fetching all stores")
         val stores = storeService.getAllStores()
+        logger.info("Found {} stores", stores.size)
         return ResponseEntity.ok(stores)
     }
 }
